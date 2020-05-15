7531a086e8469fb8ac579d47e766768d718d64dc
Bernhard Wodok <bwodok@shinen.com>
2020-05-15 17:31:44 +0200

    profiler: better handling of inlined functions

--- src/backend/profile.ts
+++ src/backend/profile.ts
@@ -23,16 +23,18 @@ export class SourceMap {
 			const tmp = path.join(os.tmpdir(), `amiga-sourcemap-${new Date().getTime()}`);
 			fs.writeFileSync(tmp, str);
 
-			const objdump = childProcess.spawnSync(this.addr2linePath, [`--exe=${this.executable}`, `@${tmp}`]);
+			const objdump = childProcess.spawnSync(this.addr2linePath, ['--functions', `--exe=${this.executable}`, `@${tmp}`]);
 			const outputs = objdump.stdout.toString().split('\n');
 			const uniqueLinesMap: Map<string, number> = new Map();
-			for(const output of outputs) {
+			for(let i = 0; i < outputs.length; i += 2) {
+				const func = outputs[i];
+				const output = outputs[i + 1];
 				let value = uniqueLinesMap.get(output);
 				if(value === undefined) {
 					const split = output.lastIndexOf(':');
 					const file = output.substr(0, split);
 					const line = parseInt(output.substr(split + 1));
-					value = this.uniqueLines.push({ file, line }) - 1;
+					value = this.uniqueLines.push({ func, file, line }) - 1;
 					uniqueLinesMap.set(output, value);
 				}
 				this.lines.push(value);
@@ -195,35 +197,21 @@ export class Profiler {
 	}
 
 	public profileFunction(): string {
-		const functions = this.symbolTable.getFunctionSymbols();
+		const functionMap: Map<string, number> = new Map();
 		const cyclesPerFunction: number[] = [];
 		const locations: SourceLine[] = [];
-		const profileArrayCopy = new Uint32Array(this.profileArray);
-		for(const func of functions) {
-			let cycles = 0;
-			for(let j = func.address >> 1; j < (func.address + func.length) >> 1; j++) {
-				cycles += profileArrayCopy[j];
-				profileArrayCopy[j] = 0;
+		for(let i = 0; i < this.profileArray.length; i++) {
+			if(this.profileArray[i] === 0)
+				continue;
+			const line = this.sourceMap.uniqueLines[this.sourceMap.lines[i]];
+			let functionId = functionMap.get(line.func);
+			if(functionId === undefined) {
+				functionId = locations.push(line) - 1;
+				cyclesPerFunction.push(0);
+				functionMap.set(line.func, functionId);
 			}
-			const funcLine = this.sourceMap.uniqueLines[this.sourceMap.lines[func.address >> 1]];
-			cyclesPerFunction.push(cycles);
-			locations.push({
-				func: func.name,
-				file: funcLine.file,
-				line: funcLine.line
-			});
+			cyclesPerFunction[functionId] += this.profileArray[i];
 		}
-
-		// not in any known function
-		let cycles = 0;
-		for(const p of profileArrayCopy) {
-			cycles += p;
-		}
-		cyclesPerFunction.push(cycles);
-		locations.push({
-			file: "",
-			line: -1
-		});
 
 		return this.profileCommon(cyclesPerFunction, locations);
 	}
