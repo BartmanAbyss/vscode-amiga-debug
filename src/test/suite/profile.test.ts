e4d840b72e4e79402a5500c3a9da0492d395354c
Bernhard Wodok <bwodok@shinen.com>
2020-05-18 16:29:29 +0200

    .

--- src/test/suite/profile.test.ts
+++ src/test/suite/profile.test.ts
@@ -12,7 +12,7 @@ suite("Profiler", () => {
 	test("unwind", () => {
 		const unwindTable = new UnwindTable(path.join(binDir, 'm68k-amiga-elf-objdump.exe'), path.join(testDataDir, 'test.elf'));
 	});
-	test("test.mingw.elf", () => {
+	test("test.elf", () => {
 		const profileBuffer = fs.readFileSync(path.join(testDataDir, 'amiga-profile-1589535143558'));
 		const profileArray = new Uint32Array(profileBuffer.buffer, profileBuffer.byteOffset, profileBuffer.length / Uint32Array.BYTES_PER_ELEMENT);
 		const codeSize = profileArray.length * 2;
@@ -24,7 +24,7 @@ suite("Profiler", () => {
 		//fs.writeFileSync(path.join(testDataDir, 'amiga-profile-1589535143558.line.amigaprofile'), profile.profileLine());
 		fs.writeFileSync(path.join(testDataDir, 'amiga-profile-1589535143558.func.amigaprofile'), profile.profileFunction());
 	});
-	test("bitshmup.mingw.elf", () => {
+	test("bitshmup.elf", () => {
 		const profileBuffer = fs.readFileSync(path.join(testDataDir, 'amiga-profile-1589800510610'));
 		const profileArray = new Uint32Array(profileBuffer.buffer, profileBuffer.byteOffset, profileBuffer.length / Uint32Array.BYTES_PER_ELEMENT);
 		const codeSize = profileArray.length * 2;
