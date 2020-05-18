62e4ed62c5002c0aee6eb56c439b6f225a5a7b31
Bernhard Wodok <bwodok@shinen.com>
2020-05-18 15:25:20 +0200

    test: added bigger test

--- src/test/suite/profile.test.ts
+++ src/test/suite/profile.test.ts
@@ -9,16 +9,28 @@ const testDataDir = path.resolve(__dirname, "../../../src/test/suite/data");
 const binDir = path.resolve(__dirname, "../../../bin/opt/bin");
 
 suite("Profiler", () => {
-	test("Profiler", () => {
+	test("test.mingw.elf", () => {
 		const profileBuffer = fs.readFileSync(path.join(testDataDir, 'amiga-profile-1589535143558'));
 		const profileArray = new Uint32Array(profileBuffer.buffer, profileBuffer.byteOffset, profileBuffer.length / Uint32Array.BYTES_PER_ELEMENT);
 		const codeSize = profileArray.length * 2;
-		const sourceMap = new SourceMap(path.join(binDir, 'm68k-amiga-elf-addr2line.exe'), path.join(testDataDir, 'a.mingw.elf'), codeSize);
-		const symbolTable = new SymbolTable(path.join(binDir, 'm68k-amiga-elf-objdump.exe'), path.join(testDataDir, 'a.mingw.elf'));
+		const sourceMap = new SourceMap(path.join(binDir, 'm68k-amiga-elf-addr2line.exe'), path.join(testDataDir, 'test.elf'), codeSize);
+		const symbolTable = new SymbolTable(path.join(binDir, 'm68k-amiga-elf-objdump.exe'), path.join(testDataDir, 'test.elf'));
 
 		const profile = new Profiler(sourceMap, symbolTable, profileArray);
 		//fs.writeFileSync(path.join(testDataDir, 'amiga-profile-1589535143558.asm.amigaprofile'), profile.profileAsm());
 		//fs.writeFileSync(path.join(testDataDir, 'amiga-profile-1589535143558.line.amigaprofile'), profile.profileLine());
 		fs.writeFileSync(path.join(testDataDir, 'amiga-profile-1589535143558.func.amigaprofile'), profile.profileFunction());
+	});
+	test("bitshmup.mingw.elf", () => {
+		const profileBuffer = fs.readFileSync(path.join(testDataDir, 'amiga-profile-1589800510610'));
+		const profileArray = new Uint32Array(profileBuffer.buffer, profileBuffer.byteOffset, profileBuffer.length / Uint32Array.BYTES_PER_ELEMENT);
+		const codeSize = profileArray.length * 2;
+		const sourceMap = new SourceMap(path.join(binDir, 'm68k-amiga-elf-addr2line.exe'), path.join(testDataDir, 'bitshmup.elf'), codeSize);
+		const symbolTable = new SymbolTable(path.join(binDir, 'm68k-amiga-elf-objdump.exe'), path.join(testDataDir, 'bitshmup.elf'));
+
+		const profile = new Profiler(sourceMap, symbolTable, profileArray);
+		//fs.writeFileSync(path.join(testDataDir, 'amiga-profile-1589800510610.asm.amigaprofile'), profile.profileAsm());
+		//fs.writeFileSync(path.join(testDataDir, 'amiga-profile-1589800510610.line.amigaprofile'), profile.profileLine());
+		fs.writeFileSync(path.join(testDataDir, 'amiga-profile-1589800510610.func.amigaprofile'), profile.profileFunction());
 	});
 });
