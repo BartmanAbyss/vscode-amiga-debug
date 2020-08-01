81a741cf82a1c063707efa7c8ca2c70dae1c7487
Bartman/Abyss <bwodok@shinen.com>
2020-08-01 19:30:25 +0200

    assembly jump visualization WIP

--- src/client/objdump.tsx
+++ src/client/objdump.tsx
@@ -2,6 +2,7 @@ import { VsCodeApi } from "./vscodeApi";
 import styles from './objdump.module.css';
 import { Scrollable } from "./scrollable";
 import { GetCycles, GetJump, JumpType } from "./68k";
+import { DiffieHellman } from "crypto";
 
 // messages from webview to vs code
 export interface IOpenDocumentMessage {
@@ -35,6 +36,7 @@ class ObjdumpView {
 			if(curRow.length === 0)
 				return;
 			const row = document.createElement('div');
+			row.className = styles.row;
 			row.attributes['data-row'] = this.rows.length;
 			row.innerText = curRow.join('\n');
 			this.container.appendChild(row);
@@ -43,12 +45,29 @@ class ObjdumpView {
 			curRow = [];
 		};
 
+		interface JumpInfo {
+			start: number[];
+			end: number;
+			level: number;
+			type: JumpType;
+		}
+
+		const jumps: JumpInfo[] = [];
+		const pcMap = new Map<number, number>();
+		let lineCount = 0;
+
 		// merge lines by same location
 		for(const line of lines) {
-			const locMatch = line.match(/^(\S.+):([0-9]+)( \(discriminator [0-9]+\))?$/);
-			if(locMatch || line.length === 0)
-				addCurRow();
+			const funcMatch = line.match(/([0-9a-f]+) <(.*)>:$/); // 00000000 <_start>:
+			if(funcMatch) {
+				// TEST
+//				if(funcMatch[2] === 'main')
+//					break;
+			}
 
+			const locMatch = line.match(/^(\S.+):([0-9]+)( \(discriminator [0-9]+\))?$/);
+			//if(locMatch || line.length === 0) // TEST
+				addCurRow();
 			if(locMatch) {
 				location = {
 					file: locMatch[1],
@@ -59,28 +78,143 @@ class ObjdumpView {
 			const insnMatch = line.match(/^ *([0-9a-f]+):\t((?:[0-9a-f]{4} )*)\s*(.*)$/); //      cce:	0c40 a00e      	cmpi.w #-24562,d0
 			if(insnMatch) {
 				const pc = parseInt(insnMatch[1], 16);
+				pcMap.set(pc, lineCount);
 				const hex = insnMatch[2].split(' ');
 				const insn = new Uint16Array(hex.length);
 				hex.forEach((h, i) => { insn[i] = parseInt(h, 16); });
 				const jump = GetJump(pc, insn);
+				if(jump) {
+					const jumpInfo = jumps.find((j) => j.end === jump.target && j.type === jump.type);
+					if(jumpInfo)
+						jumpInfo.start.push(pc);
+					else
+						jumps.push({ start: [pc], end: jump.target, level: -1, type: jump.type });
+				}
 				const cycles = GetCycles(insn);
 				let cyclesText = '';
 				if(cycles)
 					cyclesText = cycles.map((c) => `${c.total}`).join('-');
-				curRow.push(`${cyclesText.padStart(7, ' ')} ${pc.toString(16).padStart(8, ' ')}: ${insnMatch[2]} ${insnMatch[3]}${jump ? ` =>${jump.target.toString(16)} ${JumpType[jump.type]}` : ''}`);
+				curRow.push(`${cyclesText.padStart(7, ' ')} ${pc.toString(16).padStart(8, ' ')}: ${insnMatch[2]} ${insnMatch[3]}`);
+				lineCount++;
 			} else {
 				curRow.push(line.length > 0 ? line : '\u200b');
+				lineCount++;
 			}
 		}
 		addCurRow();
+
+		const sortedJumps = jumps.sort((a: JumpInfo, b: JumpInfo) => {
+			const aMin  = Math.min(...a.start, a.end);
+			const bMin  = Math.min(...b.start, b.end);
+			const aSize = Math.max(...a.start, a.end) - aMin;
+			const bSize = Math.max(...b.start, b.end) - bMin;
+			if(aSize === bSize)
+				return aMin - bMin;
+			else
+				return aSize - bSize;
+		});
+
+		function jumpIntersects(a: JumpInfo, b: JumpInfo): boolean {
+			const aMin = Math.min(...a.start, a.end);
+			const bMin = Math.min(...b.start, b.end);
+			const aMax = Math.max(...a.start, a.end);
+			const bMax = Math.max(...b.start, b.end);
+			return aMax >= bMin && aMin <= bMax;
+		}
+
+		let lastJump = 0;
+		let maxLevel = -1;
+		while(lastJump < sortedJumps.length) {
+			// The last jump is part of the next group
+			const base = lastJump;
+			// Increment level
+			sortedJumps[base].level = ++maxLevel;
+			// Find jumps that can be combined on the same level, with the largest jumps tested first.
+			// This has the advantage that large jumps are on lower levels and do not intersect with small
+			// jumps that get grouped on higher levels.
+			let exchangeItem = lastJump + 1;
+			for(let it = exchangeItem; it < sortedJumps.length; it++) {
+				// test if the jump intersects with any jump from current group
+				let ok = true;
+				for(let itCollision = base; itCollision !== exchangeItem; itCollision++) {
+					// this jump intersects so we leave it out
+					if(jumpIntersects(sortedJumps[itCollision], sortedJumps[it])) {
+						ok = false;
+						break;
+					}
+				}
+				// add jump to group
+				if(ok) {
+					// move current element to the front
+					if(it !== exchangeItem) {
+						// hmm.. this code is not good
+						[sortedJumps[exchangeItem], sortedJumps[it]] = [sortedJumps[it], sortedJumps[exchangeItem]];
+						lastJump = it;
+					} else {
+						lastJump = exchangeItem;
+						exchangeItem++;
+					}
+					sortedJumps[lastJump].level = maxLevel;
+				}
+			}
+			lastJump = exchangeItem; // move to next group
+		}
+
+		//console.log(jumps);
+		//console.log(sortedJumps);
+		
+		const svgNs = 'http://www.w3.org/2000/svg';
+		function createSvg() {
+			return document.createElementNS(svgNs, 'svg');
+		}
+		function svgNode(name: string, values: { [x: string]: string; }) {
+			const n = document.createElementNS(svgNs, name);
+			for(const p in values)
+				n.setAttributeNS(null, p, values[p]);
+			return n;
+		}
+
+		const stroke = { 'stroke': 'white', 'stroke-width': '1', 'fill': 'none' };
+		const strokeConditional = { ...stroke, 'stroke-dasharray': '2' };
+		const rowHeight = 20;
+		const rowMiddle = 6;
+		const right = 150; // needs to match CSS
+		const levelIndent = 5;
+
+		console.log(sortedJumps);
+
+		for(const jump of sortedJumps) {
+			// ignore jump if any address is unknown
+			if([...jump.start, jump.end].some((pc) => !pcMap.has(pc)))
+				continue;
+
+			const min  = pcMap.get(Math.min(...jump.start, jump.end));
+			const max  = pcMap.get(Math.max(...jump.start, jump.end));
+			const end  = pcMap.get(jump.end) - min;
+			const size = max - min + 1;
+			const indent = right - 10 - jump.level * levelIndent;
+			const svg = createSvg();
+			svg.classList.add(styles.jump);
+			svg.style.top = (min * rowHeight) + 'px';
+			svg.style.height = (size * rowHeight) + 'px';
+			const endY = end * rowHeight + rowMiddle;
+			for(const startPc of jump.start) {
+				const start = pcMap.get(startPc) - min;
+				const y = start * rowHeight + rowMiddle;
+				svg.appendChild(svgNode('polyline', { points: `${right},${y} ${indent},${y} ${indent},${endY} ${right - 4},${endY}`, ...stroke, ...(jump.type === JumpType.ConditionalBranch ? strokeConditional : {}) }));
+			}
+			svg.appendChild(svgNode('path', { d: `M${right},${endY} l-4,-4 l0,8 z`, fill: 'white' })); // arrowhead
+			this.container.appendChild(svg);
+		}
+
 		document.body.appendChild(this.container);
 	}
 
 	private selectRow(nextRow: number, scroll: boolean) {
 		if(nextRow === this.curRow)
 			return;
-		this.rows[this.curRow].className = '';
-		this.rows[nextRow].className = styles.cur;
+		this.rows[this.curRow].classList.remove(styles.cur);
+		this.rows[nextRow].classList.add(styles.cur);
 		if(scroll) {
 			const scrollTo = this.rows[nextRow].offsetTop - document.documentElement.clientHeight / 2;
 			if(Math.abs(nextRow - this.curRow) > 1)
