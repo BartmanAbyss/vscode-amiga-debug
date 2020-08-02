8807fa5b85fcd11b57dc4768bc42eb4ba8e7d8ff
Bartman/Abyss <bwodok@shinen.com>
2020-08-02 13:52:09 +0200

    68k jumps isolate per function

--- src/client/objdump.tsx
+++ src/client/objdump.tsx
@@ -2,7 +2,6 @@ import { VsCodeApi } from "./vscodeApi";
 import styles from './objdump.module.css';
 import { Scrollable } from "./scrollable";
 import { GetCycles, GetJump, JumpType } from "./68k";
-import { DiffieHellman } from "crypto";
 
 // messages from webview to vs code
 export interface IOpenDocumentMessage {
@@ -16,6 +15,13 @@ interface Location {
 	line: number;
 }
 
+interface JumpInfo {
+	start: number[];
+	end: number;
+	level: number;
+	type: JumpType;
+}
+
 declare const OBJDUMP: string;
 
 class ObjdumpView {
@@ -23,6 +29,8 @@ class ObjdumpView {
 	private scroller: Scrollable;
 	private rows: HTMLDivElement[] = [];
 	private locations: Location[] = [];
+	private jumps: JumpInfo[] = [];
+	private pcMap = new Map<number, number>();
 	private curRow = 0;
 
 	private create() {
@@ -32,6 +40,7 @@ class ObjdumpView {
 		const lines = OBJDUMP.replace(/\r/g, '').split('\n');
 		let location: Location = { file: '', line: -1 };
 		let curRow: string[] = [];
+		let funcJumps: JumpInfo[] = [];
 		const addCurRow = () => {
 			if(curRow.length === 0)
 				return;
@@ -44,25 +53,78 @@ class ObjdumpView {
 			this.locations.push({ ...location });
 			curRow = [];
 		};
+		const addCurFunc = (startPc: number, endPc: number) => {
+			const sortedJumps = funcJumps.filter((j) => j.end >= startPc && j.end < endPc).sort((a: JumpInfo, b: JumpInfo) => {
+				const aMin  = Math.min(...a.start, a.end);
+				const bMin  = Math.min(...b.start, b.end);
+				const aSize = Math.max(...a.start, a.end) - aMin;
+				const bSize = Math.max(...b.start, b.end) - bMin;
+				if(aSize === bSize)
+					return aMin - bMin;
+				else
+					return aSize - bSize;
+			});
+	
+			function jumpIntersects(a: JumpInfo, b: JumpInfo): boolean {
+				const aMin = Math.min(...a.start, a.end);
+				const bMin = Math.min(...b.start, b.end);
+				const aMax = Math.max(...a.start, a.end);
+				const bMax = Math.max(...b.start, b.end);
+				return aMax >= bMin && aMin <= bMax;
+			}
+	
+			let lastJump = 0;
+			let maxLevel = -1;
+			while(lastJump < sortedJumps.length) {
+				// The last jump is part of the next group
+				const base = lastJump;
+				// Increment level
+				sortedJumps[base].level = ++maxLevel;
+				// Find jumps that can be combined on the same level, with the largest jumps tested first.
+				// This has the advantage that large jumps are on lower levels and do not intersect with small
+				// jumps that get grouped on higher levels.
+				let exchangeItem = lastJump + 1;
+				for(let it = exchangeItem; it < sortedJumps.length; it++) {
+					// test if the jump intersects with any jump from current group
+					let ok = true;
+					for(let itCollision = base; itCollision !== exchangeItem; itCollision++) {
+						// this jump intersects so we leave it out
+						if(jumpIntersects(sortedJumps[itCollision], sortedJumps[it])) {
+							ok = false;
+							break;
+						}
+					}
+					// add jump to group
+					if(ok) {
+						// move current element to the front
+						if(it !== exchangeItem) {
+							// hmm.. this code is not good
+							[sortedJumps[exchangeItem], sortedJumps[it]] = [sortedJumps[it], sortedJumps[exchangeItem]];
+							lastJump = it;
+						} else {
+							lastJump = exchangeItem;
+							exchangeItem++;
+						}
+						sortedJumps[lastJump].level = maxLevel;
+					}
+				}
+				lastJump = exchangeItem; // move to next group
+			}
 
-		interface JumpInfo {
-			start: number[];
-			end: number;
-			level: number;
-			type: JumpType;
-		}
+			this.jumps.push(...sortedJumps);
+			funcJumps = [];
+		};
 
-		const jumps: JumpInfo[] = [];
-		const pcMap = new Map<number, number>();
 		let lineCount = 0;
+		let funcStartPc = 0;
 
 		// merge lines by same location
 		for(const line of lines) {
 			const funcMatch = line.match(/([0-9a-f]+) <(.*)>:$/); // 00000000 <_start>:
 			if(funcMatch) {
-				// TEST
-//				if(funcMatch[2] === 'main')
-//					break;
+				const funcEndPc = parseInt(funcMatch[1], 16);
+				addCurFunc(funcStartPc, funcEndPc);
+				funcStartPc = funcEndPc;
 			}
 
 			const locMatch = line.match(/^(\S.+):([0-9]+)( \(discriminator [0-9]+\))?$/);
@@ -78,23 +140,23 @@ class ObjdumpView {
 			const insnMatch = line.match(/^ *([0-9a-f]+):\t((?:[0-9a-f]{4} )*)\s*(.*)$/); //      cce:	0c40 a00e      	cmpi.w #-24562,d0
 			if(insnMatch) {
 				const pc = parseInt(insnMatch[1], 16);
-				pcMap.set(pc, lineCount);
+				this.pcMap.set(pc, lineCount);
 				const hex = insnMatch[2].split(' ');
 				const insn = new Uint16Array(hex.length);
 				hex.forEach((h, i) => { insn[i] = parseInt(h, 16); });
 				const jump = GetJump(pc, insn);
 				if(jump) {
-					const jumpInfo = jumps.find((j) => j.end === jump.target && j.type === jump.type);
+					const jumpInfo = funcJumps.find((j) => j.end === jump.target && j.type === jump.type);
 					if(jumpInfo)
 						jumpInfo.start.push(pc);
 					else
-						jumps.push({ start: [pc], end: jump.target, level: -1, type: jump.type });
+						funcJumps.push({ start: [pc], end: jump.target, level: -1, type: jump.type });
 				}
 				const cycles = GetCycles(insn);
 				let cyclesText = '';
 				if(cycles)
 					cyclesText = cycles.map((c) => `${c.total}`).join('-');
-				curRow.push(`${cyclesText.padStart(7, ' ')} ${pc.toString(16).padStart(8, ' ')}: ${insnMatch[2]} ${insnMatch[3]}`);
+				curRow.push(`${cyclesText.padStart(7, ' ')} ${pc.toString(16).padStart(8, ' ')}: ${insnMatch[3]}`);
 				lineCount++;
 			} else {
 				curRow.push(line.length > 0 ? line : '\u200b');
@@ -102,67 +164,8 @@ class ObjdumpView {
 			}
 		}
 		addCurRow();
+		addCurFunc(funcStartPc, 0x7fffffff);
 
-		const sortedJumps = jumps.sort((a: JumpInfo, b: JumpInfo) => {
-			const aMin  = Math.min(...a.start, a.end);
-			const bMin  = Math.min(...b.start, b.end);
-			const aSize = Math.max(...a.start, a.end) - aMin;
-			const bSize = Math.max(...b.start, b.end) - bMin;
-			if(aSize === bSize)
-				return aMin - bMin;
-			else
-				return aSize - bSize;
-		});
-
-		function jumpIntersects(a: JumpInfo, b: JumpInfo): boolean {
-			const aMin = Math.min(...a.start, a.end);
-			const bMin = Math.min(...b.start, b.end);
-			const aMax = Math.max(...a.start, a.end);
-			const bMax = Math.max(...b.start, b.end);
-			return aMax >= bMin && aMin <= bMax;
-		}
-
-		let lastJump = 0;
-		let maxLevel = -1;
-		while(lastJump < sortedJumps.length) {
-			// The last jump is part of the next group
-			const base = lastJump;
-			// Increment level
-			sortedJumps[base].level = ++maxLevel;
-			// Find jumps that can be combined on the same level, with the largest jumps tested first.
-			// This has the advantage that large jumps are on lower levels and do not intersect with small
-			// jumps that get grouped on higher levels.
-			let exchangeItem = lastJump + 1;
-			for(let it = exchangeItem; it < sortedJumps.length; it++) {
-				// test if the jump intersects with any jump from current group
-				let ok = true;
-				for(let itCollision = base; itCollision !== exchangeItem; itCollision++) {
-					// this jump intersects so we leave it out
-					if(jumpIntersects(sortedJumps[itCollision], sortedJumps[it])) {
-						ok = false;
-						break;
-					}
-				}
-				// add jump to group
-				if(ok) {
-					// move current element to the front
-					if(it !== exchangeItem) {
-						// hmm.. this code is not good
-						[sortedJumps[exchangeItem], sortedJumps[it]] = [sortedJumps[it], sortedJumps[exchangeItem]];
-						lastJump = it;
-					} else {
-						lastJump = exchangeItem;
-						exchangeItem++;
-					}
-					sortedJumps[lastJump].level = maxLevel;
-				}
-			}
-			lastJump = exchangeItem; // move to next group
-		}
-
-		//console.log(jumps);
-		//console.log(sortedJumps);
-		
 		const svgNs = 'http://www.w3.org/2000/svg';
 		function createSvg() {
 			return document.createElementNS(svgNs, 'svg');
@@ -181,16 +184,16 @@ class ObjdumpView {
 		const right = 150; // needs to match CSS
 		const levelIndent = 5;
 
-		console.log(sortedJumps);
+		console.log(this.jumps);
 
-		for(const jump of sortedJumps) {
+		for(const jump of this.jumps) {
 			// ignore jump if any address is unknown
-			if([...jump.start, jump.end].some((pc) => !pcMap.has(pc)))
+			if([...jump.start, jump.end].some((pc) => !this.pcMap.has(pc)))
 				continue;
 
-			const min  = pcMap.get(Math.min(...jump.start, jump.end));
-			const max  = pcMap.get(Math.max(...jump.start, jump.end));
-			const end  = pcMap.get(jump.end) - min;
+			const min  = this.pcMap.get(Math.min(...jump.start, jump.end));
+			const max  = this.pcMap.get(Math.max(...jump.start, jump.end));
+			const end  = this.pcMap.get(jump.end) - min;
 			const size = max - min + 1;
 			const indent = right - 10 - jump.level * levelIndent;
 			const svg = createSvg();
@@ -199,7 +202,7 @@ class ObjdumpView {
 			svg.style.height = (size * rowHeight) + 'px';
 			const endY = end * rowHeight + rowMiddle;
 			for(const startPc of jump.start) {
-				const start = pcMap.get(startPc) - min;
+				const start = this.pcMap.get(startPc) - min;
 				const y = start * rowHeight + rowMiddle;
 				svg.appendChild(svgNode('polyline', { points: `${right},${y} ${indent},${y} ${indent},${endY} ${right - 4},${endY}`, ...stroke, ...(jump.type === JumpType.ConditionalBranch ? strokeConditional : {}) }));
 			}
