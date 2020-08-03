3d1fa525a8561024e7bafd66f51a4be26beacd42
Bartman/Abyss <bwodok@shinen.com>
2020-08-03 22:25:51 +0200

    jump visualization in assembly

--- src/client/debugger/assembly.tsx
+++ src/client/debugger/assembly.tsx
@@ -13,23 +13,31 @@ import { DropdownOptionProps, DropdownComponent } from '../dropdown';
 
 import { Icon } from '../icons';
 import * as SymbolMethod from '../icons/symbol-method.svg';
-import { ObjdumpModel, Line, Function } from '../objdump';
+import { ObjdumpModel, Line, Function, JumpInfo } from '../objdump';
+import { JumpType } from '../68k';
 
 // based on https://github.com/developit/preact-virtual-list/blob/master/src/index.js
 
 const STYLE_INNER = 'position:relative; overflow:hidden; width:100%; min-height:100%;';
 const STYLE_CONTENT = 'position:absolute; top:0; left:0; height:100%; width:100%; overflow:visible;';
 
-interface VirtualListProps<R> {
-	data: R[];
-	renderRow: (a: R) => JSX.Element;
+interface Absolute {
+	top: number;
+	height: number;
+}
+
+interface VirtualListProps<R, A extends Absolute> {
+	rows: R[];
+	renderRow: (row: R) => JSX.Element;
 	rowHeight: number;
+	absolutes?: A[];
+	renderAbsolute?: (absolute: A, top: number, bottom: number) => JSX.Element;
 	overscanCount?: number;
 	sync?: boolean;
 	[x: string]: any; // passthrough properties
 }
 
-export default class VirtualList<R> extends Component<VirtualListProps<R>, { offset: number; height: number }> {
+export default class VirtualList<R, A extends Absolute> extends Component<VirtualListProps<R, A>, { offset: number; height: number }> {
 	private resize = () => {
 		if (this.state.height !== (this.base as HTMLElement).offsetHeight) {
 			this.setState({ height: (this.base as HTMLElement).offsetHeight });
@@ -54,7 +62,7 @@ export default class VirtualList<R> extends Component<VirtualListProps<R>, { off
 		removeEventListener('resize', this.resize);
 	}
 
-	public render({ data, rowHeight, renderRow, overscanCount=10, sync, ...props }: VirtualListProps<R>, { offset=0, height=0 }) {
+	public render({ rows, rowHeight, renderRow, absolutes, renderAbsolute, overscanCount=10, sync, ...props }: VirtualListProps<R, A>, { offset=0, height=0 }) {
 		// first visible row index
 		let start = (offset / rowHeight)|0;
 
@@ -72,11 +80,12 @@ export default class VirtualList<R> extends Component<VirtualListProps<R>, { off
 		const end = start + 1 + visibleRowCount;
 
 		// data slice currently in viewport plus overscan items
-		const selection = data.slice(start, end);
+		const selection = rows.slice(start, end);
 
 		return (
 			<div onScroll={this.handleScroll} {...props}>
-				<div style={`${STYLE_INNER} height:${data.length*rowHeight}px;`}>
+				<div style={`${STYLE_INNER} height:${rows.length*rowHeight}px;`}>
+					{absolutes && absolutes.filter((a) => a.top < end*rowHeight && a.top + a.height >= start*rowHeight).map((a) => renderAbsolute(a, start*rowHeight, end*rowHeight))}
 					<div style={`${STYLE_CONTENT} top:${start*rowHeight}px;`}>
 						{selection.map(renderRow)}
 					</div>
@@ -86,7 +95,9 @@ export default class VirtualList<R> extends Component<VirtualListProps<R>, { off
 	}
 }
 
-class VirtualListLine extends VirtualList<Line> {}
+interface JumpAbsolute extends JumpInfo, Absolute {}
+
+class VirtualListLine extends VirtualList<Line, JumpAbsolute> {}
 
 declare const MODELS: IProfileModel[];
 
@@ -107,7 +118,10 @@ export const AssemblyView: FunctionComponent<{
 	frame: number,
 	time: number
 }> = ({ frame, time }) => {
-	const [content, functions] = useMemo(() => {
+	const cssVariables = useCssVariables();
+	const height = parseInt(cssVariables['editor-font-size']) + 3; // needs to match CSS
+
+	const [content, functions, jumps] = useMemo(() => {
 		const textSection = MODELS[0].amiga.sections.find((section) => section.name === '.text');
 		const hits = new Array<number>(textSection.size >> 1).fill(0);
 		const cycles = new Array<number>(textSection.size >> 1).fill(0);
@@ -120,8 +134,30 @@ export const AssemblyView: FunctionComponent<{
 		}
 
 		const model = new ObjdumpModel(MODELS[0].amiga.objdump, hits, cycles);
-		return [model.content, model.functions.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()))];
-	}, [frame]);
+		const pcMap = new Map<number, number>(); // pc -> row index
+		model.content.forEach((line, index) => {
+			if(line.pc !== undefined)
+				pcMap.set(line.pc, index);
+		});
+
+		const jumps: JumpAbsolute[] = model.jumps.filter((jump) =>
+			[...jump.start, jump.end].every((pc) => pcMap.has(pc))
+		).map((jump) => {
+			const min  = pcMap.get(Math.min(...jump.start, jump.end));
+			const max  = pcMap.get(Math.max(...jump.start, jump.end));
+			return {
+				start: jump.start.map((j) => pcMap.get(j)),
+				end: pcMap.get(jump.end),
+				level: jump.level,
+				type: jump.type,
+				top: min * height,
+				height: (max - min + 1) * height
+			};
+		});
+		console.log(jumps);
+
+		return [model.content, model.functions.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase())), jumps];
+	}, [frame, height]);
 
 	const [pc, func] = useMemo(() => {
 		const pcTrace = MODELS[frame].amiga.pcTrace;
@@ -154,9 +190,6 @@ export const AssemblyView: FunctionComponent<{
 		});
 	}, []);
 
-	const cssVariables = useCssVariables();
-	const height = parseInt(cssVariables['editor-font-size']) + 3; // needs to match CSS
-
 	const renderRow = useCallback((c: Line) => (
 		c.pc === undefined
 		? <div class={styles.row}>{c.text + '\n'}</div>
@@ -169,6 +202,31 @@ export const AssemblyView: FunctionComponent<{
 			{'\n'}
 		</div>		
 	), [onClick, pc]);
+
+	const renderJump = useCallback((jump: JumpAbsolute, top: number, bottom: number) => {
+		const stroke = { fill: 'none' };
+		const strokeConditional = { ...stroke, 'stroke-dasharray': '1' };
+		const right = 150; // needs to match CSS
+		const rowMiddle = 6;
+		const levelIndent = 5;
+
+		const min  = Math.min(...jump.start, jump.end);
+		const max  = Math.max(...jump.start, jump.end);
+		const end  = jump.end - min;
+		const size = max - min + 1;
+		const indent = right - 10 - jump.level * levelIndent;
+		const endY = end * height + rowMiddle;
+
+		return (<svg class={styles.jump} style={{top: jump.top + 'px', height: jump.height + 'px'}}>
+			{jump.start.map((startRow) => {
+				const start = startRow - min;
+				const y = start * height + rowMiddle;
+				
+				return <polyline points={`${right},${y} ${indent},${y} ${indent},${endY} ${right - 4},${endY}`} {...stroke} {...(jump.type === JumpType.ConditionalBranch ? strokeConditional : {})} />;
+			})}
+			<path d={`M${right},${endY} l-4,-4 l0,8 z`} stroke="none" />
+		</svg>);
+	}, [content, height]);
 
 	const listRef = useRef<Component>();
 	const [scroller, setScroller] = useState<Scrollable>(null);
@@ -199,6 +257,6 @@ export const AssemblyView: FunctionComponent<{
 			Function:&nbsp;
 			<FunctionDropdown alwaysChange={true} options={functions} value={func} onChange={onChangeFunction} />
 		</div>
-		<VirtualListLine ref={listRef} class={styles.container} data={content} renderRow={renderRow} rowHeight={height} overscanCount={10} />
+		<VirtualListLine ref={listRef} class={styles.container} rows={content} renderRow={renderRow} rowHeight={height} absolutes={jumps} renderAbsolute={renderJump} overscanCount={10} />
 	</Fragment>;
 };
