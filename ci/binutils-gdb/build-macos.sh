4ac5615f385131646b4a113a40f534aec5f6e9cf
Graham Bates <info@grahambates.com>
2022-10-30 01:09:03 +0000

    Avoid linking libintl.8.dylib
    Mac OS will always search for dynamic libraries before static ones.
    The only way to force it to use the static `libintl.a` instead is to
    delete `libintl.8.dylib` after installing the brew package.

--- ci/binutils-gdb/build-macos.sh
+++ ci/binutils-gdb/build-macos.sh
@@ -5,5 +5,11 @@ set -x
 
 export PATH="`brew --prefix bison`/bin:$PATH" # System bison is too old, use homebrew bison
 
+# Temporarily rename libintl dynamic lib to force use of static version
+mv /usr/local/opt/gettext/lib/libintl.8.dylib /usr/local/opt/gettext/lib/libintl.8.dylib.bk
+
 cd build-binutils-gdb
 make --jobs 4
+
+# Restore dylib
+mv /usr/local/opt/gettext/lib/libintl.8.dylib.bk /usr/local/opt/gettext/lib/libintl.8.dylib
