972ebadaa0e529b2bf1bb201c59535787f2ce11b
Graham Bates <info@grahambates.com>
2023-05-12 20:49:03 +0100

    update mac/linux binaries

---
+++ bin/darwin/opt/lib/gcc/m68k-amiga-elf/13.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-clocale --disable-gcov --disable-libada --disable-libgomp --disable-libsanitizer --disable-libssp --disable-libvtv --disable-multilib --disable-nls --disable-threads --enable-languages=c,c++ --enable-lto --enable-static --prefix=/Users/runner/work/vscode-amiga-debug/vscode-amiga-debug/output --target=m68k-amiga-elf --with-cpu=68000";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "mcpu=68000" } };
